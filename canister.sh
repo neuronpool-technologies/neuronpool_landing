483a7ab9a46ad08e67e016506a5b3f3a32beb9e6
jesssekeogh <jesse.keogh74@gmail.com>
2024-05-16 00:37:06 +0100

    adding custom domain

---
+++ canister.sh
@@ -0,0 +1,5 @@
+# helpful launch commands
+
+# dfx canister --network ic install --mode reinstall neuronpool_landing_frontend
+# dfx deploy --network ic neuronpool_landing_frontend
+# dfx canister --network ic status neuronpool_landing_frontend
